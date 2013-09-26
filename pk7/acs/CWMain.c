a8bb00965f589158f478f23ba1860b70aa788d16
Marrub <marrub.xz@gmail.com>
2013-09-26 07:05:47 -0400

    Fixed to work with new chaingun actor.

--- pk7/acs/CWMain.c
+++ pk7/acs/CWMain.c
@@ -6,12 +6,12 @@
 #include "CWDefs.h"
 
 #include "CWKeyFinder.h" // Keypress finder
-#include "CWAttach.h" // Attachment and detatchment
+#include "CWAttach.h"    // Attachment and detatchment
 
 function int WepSelected(void)
 {
 	if (CheckWeapon("CPistol")) return WEP_PISTOL;
-	if (CheckWeapon("Chaingun")) return WEP_CHAINGUN;
+	if (CheckWeapon("CChaingun")) return WEP_CHAINGUN;
 	if (CheckWeapon("Fist")) return WEP_FIST;
 	return WEP_NULL;
 }
