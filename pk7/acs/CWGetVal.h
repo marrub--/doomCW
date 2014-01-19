78387822411a90fb8f8881a1d24a8ad6570f750a
marrub <marrub.xz@gmail.com>
2014-01-19 04:16:38 -0500

    that looks so much better

--- pk7/acs/CWGetVal.h
+++ pk7/acs/CWGetVal.h
@@ -10,7 +10,7 @@ script "CW_GETVAL" (int arg1)
 		print(s:"Used default GETVAL value, please report this");
 	break;
 	case GV_PSTF1:
-		SetResultValue(3-pis_spdmod);
+		SetResultValue(4-pis_spdmod);
 	break;
 	case GV_PSTF2:
 		SetResultValue(5-pis_accmod);
