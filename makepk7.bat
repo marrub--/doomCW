0fa4fda2dc0c1e4c79a85ad591f94c17949c6857
marrub <marrub.xz@gmail.com>
2014-01-27 12:36:20 -0500

    Added a spawn for the pistol, changed a sound

--- makepk7.bat
+++ makepk7.bat
@@ -5,7 +5,7 @@ set proj_path=%~dp0
 set proj_path=%proj_path:~0,-1%
 set proj_path=%proj_path%\pk7\
 rem 7zip path
-set comp_path=..\_utils\peazip\res\7z\
+set comp_path=..\..\Utils\Peazip\res\7z\
 set PATH=%PATH%;%comp_path%
 7z a out.7z %proj_path%\*
 rename out.7z %proj_name%.pk7
